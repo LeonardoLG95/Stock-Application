2951a08ef6b885dd7b676ee1b4283c6668f6d4d3
Leo <leonardo-lg@hotmail.com>
2021-05-22 22:13:09 +0200

    feature: first try for represent data
    db is empty so I have to fill it with my old functions

--- stocklooker/looker/views.py
+++ stocklooker/looker/views.py
@@ -1,22 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+
 from .models import StockData
-# Create your views here.
 
 
-def index(request):
-    """
-    Función vista para la página inicio del sitio.
-    """
-    # Genera contadores de algunos de los objetos principales
-    num_rows=StockData.objects.all().count()
-    '''num_instances=BookInstance.objects.all().count()
-    # Libros disponibles (status = 'a')
-    num_instances_available=BookInstance.objects.filter(status__exact='a').count()
-    num_authors=Author.objects.count()  # El 'all()' esta implícito por defecto.'''
+class StockListView(generic.ListView):
+    model = StockData
 
-    # Renderiza la plantilla HTML index.html con los datos en la variable contexto
-    return render(
-        request,
-        'index.html',
-        context={'num_rows': num_rows},
-    )
+    def get_queryset(self):
+        return StockData.objects.filter().distinct('ticker')
