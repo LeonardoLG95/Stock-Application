e6a5a704ecbac5c5ef1eccd201ec226f0cb63ee9
leo <leonardolg95m@gmail.com>
2022-04-02 22:03:25 +0200

    refactor(old folders): cleaning old idea
    now use superset

--- django/stocklooker/looker/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^stock_list$', views.stock_list, name='stock_list'),
-    url(r'^update$', views.update, name='update')
-]
